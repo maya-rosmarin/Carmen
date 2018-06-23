36f2cb7c6b3822f1b9301b53672ee59da56de031
Joey Wu <wjoeyu@gmail.com>
2018-06-23 18:48:41 -0400

    added scroll to city tiles

--- frontend/src/components/cities.jsx
+++ frontend/src/components/cities.jsx
@@ -3,9 +3,30 @@ import React from 'react';
 export const cityTiles = () => {
     return (
         <div className="city-tile">
+        <div className="city-image-wrapper">
+          <div>New York</div>
           <img className="city-image" src="https://cimg1.ibsrv.net/cimg/www.fodors.com/2000x2000_60-1/273/NewYork-Hero-2000pix-212273.jpg" alt="new york" />
-          <img className="city-image" src="https://media.brides.com/photos/59a816ce75cc893b2c4ea6a5/1:1/w_767/tokyo-GettyImages-539678562.jpgg" alt="tokyo" />
-          <img className="city-image" src="https://www.klm.com/travel/en/images/AF5CEFCC-29C4-4F75-9460-83522D99FA18_tcm493-517436_456x456_80.jpg" alt="paris" />
+        </div>
+        <div className="city-image-wrapper">
+          <div>Tokyo
+        </div>  <img className="city-image" src="https://media.brides.com/photos/59a816ce75cc893b2c4ea6a5/1:1/w_767/tokyo-GettyImages-539678562.jpgg" alt="tokyo" />
+        </div>
+        <div className="city-image-wrapper">
+          <div>Paris
+        </div>  <img className="city-image" src="https://www.klm.com/travel/en/images/AF5CEFCC-29C4-4F75-9460-83522D99FA18_tcm493-517436_456x456_80.jpg" alt="paris" />
+        </div>
+        <div className="city-image-wrapper">
+          <div>New York</div>
+          <img className="city-image" src="https://www.airfrance.us/US/common/common/img/tbaf/destinations/NYC/NYC-1_1-768x768.jpg" alt="newyork-2" />
+        </div>
+        <div className="city-image-wrapper">
+          <div>Tokyo
+        </div>  <img className="city-image" src="https://i.pinimg.com/originals/56/1f/b2/561fb2c1f62490b5909a61b9f9c6cc01.jpg" alt="tokyo-2" />
+        </div>
+        <div className="city-image-wrapper">
+          <div>Paris
+        </div>  <img className="city-image" src="https://10mosttoday.com/wp-content/uploads/2013/07/arcDeTriumph.jpg" alt="paris-2" />
+        </div>
         </div>
     );
 };
