6073380632a4da1c7de3d5c6e6146abfb1504ad7
Joey <wjoeyu@gmail.com>
2018-06-27 15:58:19 -0400

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Questionnaire from './components/questionnaire/questionnaire';
 import Itinerary from './components/itinerary/itinerary';
+import { Route } from 'react-router-dom';
 import './assets/stylesheets/question.css';
 import './assets/stylesheets/itinerary.css';
 
@@ -8,8 +9,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Questionnaire />
-        <Itinerary />
+        <Route exact path="/" component={Questionnaire} />
+        <Route exact path="/itinerary" component={Itinerary} />
       </div>
     );
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import configureStore from './store/store';
 import registerServiceWorker from './registerServiceWorker';
@@ -7,6 +8,6 @@ import registerServiceWorker from './registerServiceWorker';
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<App store={store} />, root);
+  ReactDOM.render(<HashRouter><App store={store} /></HashRouter>, root);
   registerServiceWorker();
 })
