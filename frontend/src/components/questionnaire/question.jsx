cc5362600c7558c801c444dda034f6c75ee9da93
Joey <wjoeyu@gmail.com>
2018-07-04 02:14:58 -0400

    added auto scroll onsubmit for questionnaire

--- frontend/src/components/questionnaire/question.jsx
+++ frontend/src/components/questionnaire/question.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { cityTiles } from './cities';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 class Question extends React.Component {
   constructor (props) {
@@ -10,17 +10,25 @@ class Question extends React.Component {
 
   handleSubmit (e) {
     e.preventDefault();
-    alert('Good choice')
+    // alert('Good choice');
+    if (this.props.id === "where") {
+      document.getElementById('how-long').scrollIntoView( {behavior: "smooth"});
+    } else if (this.props.id === "how-long") {
+      document.getElementById('how-much').scrollIntoView( {behavior: "smooth"});
+    } else if (this.props.id === "how-much") {
+      this.props.history.push('/itinerary');
+    }
   }
 
   render () {
     return (
-      <div className="question-form">
+      <div className="question-form" id={this.props.id}>
         <div className="question-text">
           <h1 className="question-text-item">{this.props.text}</h1>
           <form onSubmit={this.handleSubmit}>
             <input className="questionnaire-input" type="text"></input>
           </form>
+
          {this.props.text === "Where would you like to go?" ? cityTiles() : "" }
 
          {this.props.text === "What's your budget?" ?
@@ -32,4 +40,4 @@ class Question extends React.Component {
   }
 }
 
-export default Question;
+export default withRouter(Question);
--- frontend/src/components/questionnaire/questionnaire.jsx
+++ frontend/src/components/questionnaire/questionnaire.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Question from './question';
+import { HashLink as Link } from 'react-router-hash-link';
+
 
 class Questionnaire extends React.Component {
   render () {
     return (
       <div>
-        <Question text={"Where would you like to go?"}/>
-        <Question text={"What's your timeline?"}/>
-        <Question text={"What's your budget?"}/>
+        <Question id={"where"} text={"Where would you like to go?"}/>
+        <Question id={"how-long"} text={"What's your timeline?"}/>
+        <Question id={"how-much"} text={"What's your budget?"}/>
       </div>
     )
   }
