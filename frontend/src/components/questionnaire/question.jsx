8e18ed4994ac54a892798971012921c92e8dee54
Joey <wjoeyu@gmail.com>
2018-07-05 08:51:43 -0400

    added focus and scroll

--- frontend/src/components/questionnaire/question.jsx
+++ frontend/src/components/questionnaire/question.jsx
@@ -12,8 +12,10 @@ class Question extends React.Component {
     e.preventDefault();
     // alert('Good choice');
     if (this.props.id === "where") {
+      document.getElementById('how-long-input').focus( {preventScroll:true} );
       document.getElementById('how-long').scrollIntoView( {behavior: "smooth"});
     } else if (this.props.id === "how-long") {
+      document.getElementById('how-much-input').focus( {preventScroll:true} );
       document.getElementById('how-much').scrollIntoView( {behavior: "smooth"});
     } else if (this.props.id === "how-much") {
       this.props.history.push('/itinerary');
@@ -26,13 +28,17 @@ class Question extends React.Component {
         <div className="question-text">
           <h1 className="question-text-item">{this.props.text}</h1>
           <form onSubmit={this.handleSubmit}>
-            <input className="questionnaire-input" type="text"></input>
+            <input
+              className="questionnaire-input"
+              type="text"
+              id={this.props.textInputId}></input>
           </form>
 
          {this.props.text === "Where would you like to go?" ? cityTiles() : "" }
 
          {this.props.text === "What's your budget?" ?
-         <Link className="leggo" to="/itinerary"><div className="flash">Let's Gooooo!</div>Let's GTFOH!</Link> : "" }
+         <Link className="leggo" to="/itinerary">
+            <div className="flash">Let's Gooooo!</div>Let's GTFOH!</Link> : "" }
 
         </div>
       </div>
--- frontend/src/components/questionnaire/questionnaire.jsx
+++ frontend/src/components/questionnaire/questionnaire.jsx
@@ -4,12 +4,18 @@ import { HashLink as Link } from 'react-router-hash-link';
 
 
 class Questionnaire extends React.Component {
+
+  componentDidMount() {
+    document.getElementById('where-input').focus();
+  }
+
   render () {
+    window.scrollTo(0, 0);
     return (
       <div>
-        <Question id={"where"} text={"Where would you like to go?"}/>
-        <Question id={"how-long"} text={"What's your timeline?"}/>
-        <Question id={"how-much"} text={"What's your budget?"}/>
+        <Question id={"where"} text={"Where would you like to go?"} textInputId={"where-input"}/>
+        <Question id={"how-long"} text={"What's your timeline?"}  textInputId={"how-long-input"}/>
+        <Question id={"how-much"} text={"What's your budget?"}  textInputId={"how-much-input"}/>
       </div>
     )
   }
