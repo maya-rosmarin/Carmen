27faaac5a49005e94c75b953ca102f8da0d8584a
Maya Rosmarin <maya.rosmarin@gmail.com>
2018-07-05 15:23:39 -0400

    Save for merge

--- backend/src/models/database.js
+++ backend/src/models/database.js
@@ -1,20 +1,9 @@
-// const user = (sequelize, DataTypes) => {
-//   const User = sequelize.define('user', {
-//     username: {
-//       type: DataTypes.STRING,
-//       unique: true,
-//     },
-//   });
-//   return User;
-// }
-//
-// module.exports = user;
-
 const pg = require('pg');
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:3000';
 
 const client = new pg.Client(connectionString);
 client.connect();
+client.query('CREATE DATABASE carmen;')
 const query = client.query(
   'CREATE TABLE users(id SERIAL PRIMARY KEY, username VARCHAR(40) not null, password_digest VARCHAR(40) not null, session_token VARCHAR(40) not null)');
   query.on('end', () => { client.end(); });
